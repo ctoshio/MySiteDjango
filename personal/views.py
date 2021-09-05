e8aa55fdc1964650c14243faec066af81f79ee39
Toshio <ctoshioyoshikawa@gmail.com>
2021-09-04 21:06:56 -0500

    9-ContextExampleTemplateFor

--- personal/views.py
+++ personal/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_screen_view(request):
-    #context = {}
+    context = {}
     #context['some_string'] = "this is some string that Im passing to the view"
     #context['some_number'] =  141251
 
-    context = {
-            'some_string': "this is some string that Im passing to the view",
-            'some_number': 123456,
+    #context = {
+    #       'some_string': "this is some string that Im passing to the view",
+    #       'some_number': 123456,
+    #}
 
-    }
+    list_of_values = []
+    list_of_values.append("first entry")
+    list_of_values.append("second entry")
+    list_of_values.append("third entry")
+    list_of_values.append("fourth entry")
+    context['list_of_values'] = list_of_values
 
     return render(request, "personal/home.html", context)
