1e06c182bbe7cfb74e39637847bf327a70cede5f
Toshio <ctoshioyoshikawa@gmail.com>
2021-09-04 20:50:24 -0500

    9-contextViewsVariables

--- personal/views.py
+++ personal/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_screen_view(request):
-    print(request.headers)
-    return render(request, "personal/home.html", {})
+    context = {}
+    context['some_string'] = "this is some string that Im passing to the view"
+    context['some_number'] =  141251
+
+    return render(request, "personal/home.html", context)
