58995c03adffa87f81ad2a0aa7bf431065fc3e73
Toshio <ctoshioyoshikawa@gmail.com>
2021-09-04 20:56:10 -0500

    9-contextExampleDictionary

--- personal/views.py
+++ personal/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_screen_view(request):
-    context = {}
-    context['some_string'] = "this is some string that Im passing to the view"
-    context['some_number'] =  141251
+    #context = {}
+    #context['some_string'] = "this is some string that Im passing to the view"
+    #context['some_number'] =  141251
+
+    context = {
+            'some_string': "this is some string that Im passing to the view",
+            'some_number': 123456,
+
+    }
 
     return render(request, "personal/home.html", context)
